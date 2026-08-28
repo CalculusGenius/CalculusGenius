43ee23030f242f3039afd9e95b2a3b40f7377798
CalculusGenius <appchemphy@gmail.com>
2026-08-28 12:06:13 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,40 +6,46 @@ import { initializeApp } from
     "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
 
-// Firebase project configuration
+// =========================================
+// FIREBASE PROJECT CONFIGURATION
+// =========================================
 
 const firebaseConfig = {
 
     apiKey:
-        "AIzaSyAQN9Xuh9WbE4ccsMmO8dGGWoiBMxwBwkE",
+        "AIzaSyAEjyOZUdSkY2k8XwlaUwGjt2q1eEr0TM8",
 
     authDomain:
-        "calculus-732a5.firebaseapp.com",
+        "calculus-afbeb.firebaseapp.com",
 
     projectId:
-        "calculus-732a5",
+        "calculus-afbeb",
 
     storageBucket:
-        "calculus-732a5.firebasestorage.app",
+        "calculus-afbeb.firebasestorage.app",
 
     messagingSenderId:
-        "174983915941",
+        "749477240428",
 
     appId:
-        "1:174983915941:web:f96942414788a71729e04c",
+        "1:749477240428:web:bf0ab3831bd32538100249",
 
     measurementId:
-        "G-G81YBFXTZY"
+        "G-5H5WZKL51Q"
 
 };
 
 
-// Initialize Firebase
+// =========================================
+// INITIALIZE FIREBASE
+// =========================================
 
 const app =
     initializeApp(firebaseConfig);
 
 
-// Export the Firebase app
+// =========================================
+// EXPORT FIREBASE APP
+// =========================================
 
 export { app };
