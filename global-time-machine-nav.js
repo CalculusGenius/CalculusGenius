583359f953593ba45514bdaced5051deb40c1c19
CalculusGenius <appchemphy@gmail.com>
2026-09-25 22:53:00 +0530

    Update Laboratory links from time-machine.html to laboratory.html

--- global-time-machine-nav.js
+++ global-time-machine-nav.js
@@ -6,8 +6,8 @@
 
 (function addLaboratoryNavigation() {
     const path = window.location.pathname.split("/").pop() || "index.html";
-    const laboratoryHref = "time-machine.html";
-    const isLaboratory = path === laboratoryHref || path === "laboratory.html";
+    const laboratoryHref = "laboratory.html";
+    const isLaboratory = path === laboratoryHref;
 
     function createLink(className = "nav-link") {
         const link = document.createElement("a");
@@ -19,14 +19,14 @@
     }
 
     const desktopNav = document.querySelector(".desktop-nav");
-    if (desktopNav && !desktopNav.querySelector('a[href="time-machine.html"], a[href="laboratory.html"]')) {
+    if (desktopNav && !desktopNav.querySelector('a[href="laboratory.html"]')) {
         const otherWebsites = desktopNav.querySelector('a[href="other-websites.html"]');
         if (otherWebsites) desktopNav.insertBefore(createLink(), otherWebsites);
         else desktopNav.appendChild(createLink());
     }
 
     const mobileMenu = document.querySelector(".mobile-menu");
-    if (mobileMenu && !mobileMenu.querySelector('a[href="time-machine.html"], a[href="laboratory.html"]')) {
+    if (mobileMenu && !mobileMenu.querySelector('a[href="laboratory.html"]')) {
         const otherWebsites = mobileMenu.querySelector('a[href="other-websites.html"]');
         if (otherWebsites) mobileMenu.insertBefore(createLink(""), otherWebsites);
         else mobileMenu.appendChild(createLink(""));
